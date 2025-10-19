0fc643c7657e1aea80ec21e0c07c9d2a42fd2411
Guru <nainiguruvinder@gmail.com>
2025-10-19 18:23:33 +0530

    backend-Module

--- variables.tf
+++ variables.tf
@@ -1,29 +1,22 @@
 variable "project" {
-  default = "Roboshop"
-  type    = string
+    default = "Roboshop"
 }
 
 variable "environment" {
-  default = "dev"
-  type    = string
+    default = "dev"
 }
 
 variable "zone_id" {
-  default = "Z08007301TGLKBZ7OY820"
-  type    = string
+    default = "Z08007301TGLKBZ7OY820"
 }
 
 variable "zone_name" {
-  default = "gurulabs.xyz"
-  type    = string
+    default = "gurulabs.xyz"
 }
-
 variable "component" {
-  description = "Name of the Roboshop component"
-  type        = string
+
 }
 
 variable "rule_priority" {
-  description = "Priority for the ALB listener rule"
-  type        = number
+    
 }
